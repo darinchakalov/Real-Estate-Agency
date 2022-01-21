a3f5bea01b15d098786bf254368b4749906d7fcf
darinchakalov <darinchakalov@hotmail.com>
2022-01-21 18:35:51 +0200

    Created user register validations

--- models/User.js
+++ models/User.js
@@ -3,17 +3,20 @@ const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
 	name: {
+		validate: [/^[A-Z][a-z]{4,} [A-Z][a-z]{3,}/i, "Please fill full name"],
 		type: String,
-		required: true,
+		required: [true, "Full name is mandatory"],
 	},
 	username: {
+		minlength: [5, "Username needs to bne at least 5 characters"],
 		unique: true,
 		type: String,
-		required: true,
+		required: [true, "Username name is mandatory"],
 	},
 	password: {
+		minlength: [5, "Password needs to bne at least 4 characters"],
 		type: String,
-		required: true,
+		required: [true, "Password name is mandatory"],
 	},
 });
 
